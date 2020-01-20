8ca23efac664956a9ec2a5b1e30009b54692cea6
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2020-01-20 15:52:59 +0600

    add added

---
+++ Simple Calculator/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Add : ICalculate
+    {
+        public double add(double input, double result)
+        {
+            return input + result;
+        }
+    }
+}
