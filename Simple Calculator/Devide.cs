45048b500ad4d08db1a314c390a2f9e8aca7b51f
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2020-01-20 16:09:08 +0600

    divide added

---
+++ Simple Calculator/Devide.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Devide : ICalculate
+    {
+        public double operation(double input, double result)
+        {
+            if(input == 0 && result == 0)
+            {
+                return 0;
+            }else if(input == 0)
+            {
+                throw new ArithmeticException("Can not divide with number 0");
+            }
+            else
+            {
+                return result / input;
+            }
+        }
+    }
+}
