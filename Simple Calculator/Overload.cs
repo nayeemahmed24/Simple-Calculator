4ad43def25f890af330206861f1b3837dec706be
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2020-01-22 11:53:31 +0600

    Vector Calculator Added

---
+++ Simple Calculator/Overload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public abstract class Overload<T> where T : Overload<T>
+    {
+        public static T operator +(Overload<T> a, T b) => a.Add(b);
+
+        public abstract T Add(T another);
+           
+    }
+}
